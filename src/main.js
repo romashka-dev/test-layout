47678bc5c9dedc19ca4bd401ac7683bc9c3b3a19
romashka-dev <romikkbox@gmail.com>
2025-02-14 16:19:29 +0200

    [chore] init project + setup basic project structure

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'normalize.css'
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <div class="layout">
+    <header class="layout__header"></header>
+    <main class="layout__main">
+      <h1>Hello Vite!</h1>
+    </main>
+  </div>
+`
